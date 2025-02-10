b66fe42e3e98dfa200ef2ab4d86ef119d8d8ae57
440g <meanwestk@gmail.com>
2025-02-10 21:22:18 +0900

    test

--- wrapper_models/test.py
+++ wrapper_models/test.py
@@ -1,29 +1,35 @@
 import torch
 import numpy as np
 
-dummy_sentence_length = 30
+dummy_sentence_length = 25
 dummy_src_tokens = [2] + [7] * (dummy_sentence_length - 1)
 dummy_prev = [7] * (dummy_sentence_length - 1) + [2]
 src_tokens_test = torch.tensor([dummy_src_tokens], dtype=torch.long)
-src_lengths_test = torch.tensor([30])
+src_lengths_test = torch.tensor([25])
 prev_output_tokens_test_with_beam = torch.tensor([dummy_prev] * 5, dtype=torch.long)
-dummy_encoder_out_length = 512
+dummy_encoder_out_length = 640
 encoder_out_test_with_beam = [[7] * dummy_encoder_out_length for _ in range(5)]
 encoder_out_test_with_beam = torch.tensor([encoder_out_test_with_beam] * dummy_sentence_length, dtype=torch.long)
 
+print("------------------------------------------------")
+print("Encoder input-----------------------------------")
 print("src_tokens_test",  src_tokens_test.shape)
+print("------------------------------------------------")
 src_tokens = src_tokens_test.numpy()
 print("src_tokens",  src_tokens.shape)
-src_tokens = src_tokens.reshape(1, 1, *src_tokens.shape)
-print("src_tokens",  src_tokens.shape)
+# src_tokens = src_tokens.reshape(1, 1, *src_tokens.shape)
+# print("src_tokens",  src_tokens.shape)
+print("------------------------------------------------")
+
+print("Decoder input-----------------------------------")
 
 print("prev_output_tokens_test_with_beam",  prev_output_tokens_test_with_beam.shape)
+print("encoder_out_test_with_beam",  encoder_out_test_with_beam.shape)
+print("------------------------------------------------")
 prev_output_tokens = prev_output_tokens_test_with_beam.numpy()
 print("prev_output_tokens",  prev_output_tokens.shape)
-print("prev_output_tokens",  prev_output_tokens.shape)
-
-print("encoder_out_test_with_beam",  encoder_out_test_with_beam.shape)
 encoder_out = encoder_out_test_with_beam.numpy()
 print("encoder_out",  encoder_out.shape)
-encoder_out = encoder_out.reshape(1, *encoder_out.shape)
-print("encoder_out",  encoder_out.shape)
+# encoder_out = encoder_out.reshape(1, *encoder_out.shape)
+# print("encoder_out",  encoder_out.shape)
+print("------------------------------------------------")
